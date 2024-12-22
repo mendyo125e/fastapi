2fe15beeac3a7d1ff9e69e48fe89f8c765b9b7d7
hieu <mendyo125e@gmail.com>
2024-12-22 20:27:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,29 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
-
+import urllib3,urllib.parse
+import random
 app = FastAPI()
-
+def random_color_by_name(testcolor):
+    if testcolor=="test":
+        color_names = [
+            "blue", "green", "orange","red", "lightblue","grey","black","white" ,"yellow" ,"purple" ,"pink" ,"brown" ,"cyan","magenta","lime","gold","teal","navy","olive","maroon"       
+        ]
+    else:
+        color_names = [
+            "blue", "green", "orange","red", "lightblue","grey","black" 
+        ]
+    # Chọn ngẫu nhiên một màu
+    return random.choice(color_names)
+def guismssystem(title,message,nologin,random_color):
+    # Khởi tạo PoolManager
+    http = urllib3.PoolManager()
+    # URL API cần gửi yêu cầu
+    url = 'https://hieuphp.name.vn/api/undetected/message.php?all=1'
+    data = {'title': f'{title}','message': f'{message}','nologin': f'{nologin}','random_color': f'{random_color}'}
+    encoded_data = json.dumps(data).encode('utf-8')
+    response = http.request( 'POST',   url,  body=encoded_data,   headers={'Content-Type': 'application/json'})
+    return {"status_code": response.status,"response": response.data.decode('utf-8')}
 def lambda_handler():
     # Cấu hình trình duyệt
     chrome_options = webdriver.ChromeOptions()
@@ -19,16 +38,38 @@ def lambda_handler():
     
     # Khởi tạo WebDriver
     try:
-        driver = webdriver.Chrome(
-            service=Service(ChromeDriverManager().install()),
-            options=chrome_options
-        )
-
-        # Mở trang web
-        driver.get("https://www.chotot.com/")
+        random_color = random_color_by_name("test")
+        chrome_options = Options()
+        
+        chrome_options.browser_version = "130"
+        chrome_options.platform_name = "Windows 10"
+        lt_options = {};
+        lt_options["username"] = "alexschmidt63ng";
+        lt_options["accessKey"] = "evq0nRPGqRSZOQtu2hcYW2xy18CgxDjUotY1vYFD491PfVxPcd";
+        lt_options["smartUI.project"] = "alexschmidt63ng";
+        lt_options["resolution"] = "1024x768";
+        lt_options["recordVideo"] = "true";
+        lt_options["browserName"] = "Chrome";
+        lt_options["w3c"] = True;
+        lt_options["selenium_version"] = "4.0.0";
+        lt_options["plugin"] = "python-python";
+        chrome_options.set_capability('LT:Options', lt_options);
+        
+        # Desired capabilities can be set directly in the options if needed
+        
+            # Initialize Remote WebDriver with command_executor and desired capabilities
+        driver = webdriver.Remote(command_executor="http://hub.lambdatest.com:80/wd/hub",options=chrome_options)
+        
+        #driver.get("https://id.chotot.com/?continue=https://chat.chotot.com/chat")
+        #time.sleep(1)
+        title = driver.title
+        message=f"oslink:{ossytem}"
+        nologin=1
+        result =guismssystem(title,message,nologin,random_color)   
+        print("click xong")    
         
         # Lấy tiêu đề trang
-        title = driver.title
+       
         
         # Đóng trình duyệt
         driver.quit()
