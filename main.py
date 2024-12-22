a95c2917895b10577a5bdca715ff824cc4f31054
hieu <mendyo125e@gmail.com>
2024-12-22 19:28:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,41 @@ from typing import Optional
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 import aiofiles
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+def lambda_handler():
+    # Cấu hình trình duyệt
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")  # Chạy chế độ không giao diện
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    
+    # Khởi tạo WebDriver
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()),
+        options=chrome_options
+    )
+
+    try:
+        # Mở trang web
+        driver.get("https://www.chotot.com/")
+        
+        # Lấy tiêu đề trang
+        title = driver.title
+        
+        # Đóng trình duyệt
+        driver.quit()
+
+        return {
+            "statusCode": 200,
+            "body": f"Title of the page is: {title}"
+        }
+    except Exception as e:
+        driver.quit()
 
 app = FastAPI()
 
@@ -17,4 +52,6 @@ async def read_index():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     print("abc")
+    lambda_handler()
+     print("cccc")
     return {"item_id123": item_id, "q": q}
